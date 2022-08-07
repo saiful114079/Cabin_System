185389f8f32f9ae8d92c75bbac24b47e279106fa
md.shiabul.cse@gmail.com <md.shiabul.cse@gmail.com>
2022-08-07 22:03:49 +0600

    updated

--- fontend_part/src/views/CabinInfo/IndexView.vue
+++ fontend_part/src/views/CabinInfo/IndexView.vue
@@ -14,26 +14,15 @@
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-7 m-auto">
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                        <th>Cabin No</th>
-                                        <th>Cabin Name</th>
-                                        <th>Cabin Details</th>
-                                        <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="item in cabinlist" :key="item.cabin_no">
-                                            <td>{{ item.cabin_no }}</td>
-                                            <td>{{ item.cabin_name }}</td>
-                                            <td>{{ item.cabin_description }}</td>
-                                            <td>
-                                                <router-link class="btn btn-primary shadow btn-xs sharp mr-1" style="color:#fff" :to="{ name: 'cabin-edit', params:{id:item.cabin_no} }">Edit</router-link>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                               <template>
+                                    <v-data-table
+                                        dense
+                                        :headers="headers"
+                                        :items="desserts"
+                                        item-key="name"
+                                        class="elevation-1"
+                                    ></v-data-table>
+                                    </template>
                                 </div>
                             </div>
                         </div>
@@ -48,8 +37,108 @@
       <!-- main form -->
   </div>
 </template>
-
 <script>
+  export default {
+    data: () => ({
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        },
+      ],
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+    }),
+  }
+</script>
+<!-- <script>
 import axios from 'axios'
 export default {
 
@@ -67,4 +156,4 @@ export default {
 
 
 }
-</script>
+</script> -->
