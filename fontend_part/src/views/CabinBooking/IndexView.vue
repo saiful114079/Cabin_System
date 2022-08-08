71ea5217b7bc4aecd9de5467601e5ebe36884af7
md.shiabul.cse@gmail.com <md.shiabul.cse@gmail.com>
2022-08-08 16:54:19 +0600

    updated

--- fontend_part/src/views/CabinBooking/IndexView.vue
+++ fontend_part/src/views/CabinBooking/IndexView.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-      <!-- main form -->
-      <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card custom__card">
-                    <div class="card-header">
-                        <h4>Cabin Booking List</h4>
-                        <router-link class="btn btn-primary menu-style" :to="{ name: 'cabin-booking-add'}">Cabin Booking</router-link> 
-                    </div>
-                    <div class="card-body">
-                        <!-- main work -->
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-10 m-auto">
-                                    <!-- <table class="table table-bordered">
+    <!-- main form -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card custom__card">
+            <div class="card-header">
+              <h4>Cabin Booking List</h4>
+              <router-link
+                class="btn btn-primary menu-style"
+                :to="{ name: 'cabin-booking-add' }"
+                >Cabin Booking</router-link
+              >
+            </div>
+            <div class="card-body">
+              <!-- main work -->
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-10 m-auto">
+                    <!-- <table class="table table-bordered">
                                         <thead>
                                             
                                         </thead>
@@ -33,77 +37,65 @@
                                         </tbody>
                                     </table> -->
 
-                                    <!-- vuetify data table -->
-                                    <v-data-table :headers="headers" :items="cabinbookinglist" :items-per-page="5" class="elevation-1">
-
-                                    
-                                    
-                                    </v-data-table>
-                                    <!-- vuetify data table -->
-                                </div>
-                            </div>
-                        </div>
-                        <!-- main part -->
-                    </div>
-                    
+                    <!-- vuetify data table -->
+                    <v-data-table
+                      :headers="headers"
+                      :items="cabinbookinglist"
+                      :items-per-page="5"
+                      class="elevation-1"
+                    ></v-data-table>
+                    <!-- vuetify data table -->
+                  </div>
                 </div>
+              </div>
+              <!-- main part -->
             </div>
-          
+          </div>
         </div>
       </div>
-      <!-- main form -->
+    </div>
+    <!-- main form -->
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Cabin Name",
+          value: "cabin_name",
+        },
+        {
+          text: "Date",
+          value: "booking_date",
+        },
+        {
+          text: "Shift",
+          value: 'shift_type == "e" ? "Evening" : "Morning" ',
+        },
+        {
+          text: "Surgeon",
+          value: "dr_name",
+        },
+        {
+          text: "Patient",
+          value: "patient_name",
+        },
+        {
+          text: "Action",
+          value: "",
+        },
+      ],
+      cabinbookinglist: [],
+    };
+  },
 
-    data(){
-      return{
-
-        headers: [
-          {
-            text: 'Cabin Name',
-            value: 'cabin_name',
-          },
-          {
-            text: 'Date',
-            value: 'booking_date',
-          },
-          {
-            text: 'Shift',
-            value: 'shift_type == "e" ? "Evening" : "Morning" ',
-          },
-          {
-            text: 'Surgeon',
-            value: 'dr_name',
-          },
-          {
-            text: 'Patient',
-            value: 'patient_name',
-          },
-          {
-            text: 'Action',
-            value: '',
-          },
-
-        ],
-        cabinbookinglist:[],
-        
-      }
-    },
-
-    async mounted() {
-      let result = await axios.get('http://127.0.0.1:8000/api/cabin-booking');
-      this.cabinbookinglist = result.data.data
-    },
-
-
-
-}
-
-
-
-
+  async mounted() {
+    let result = await axios.get("http://127.0.0.1:8000/api/cabin-booking");
+    this.cabinbookinglist = result.data.data;
+  },
+};
 </script>
--- fontend_part/src/views/CabinInfo/IndexView.vue
+++ fontend_part/src/views/CabinInfo/IndexView.vue
@@ -1,70 +1,79 @@
 <template>
   <div>
-      <!-- main form -->
-      <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card custom__card">
-                    <div class="card-header">
-                        <h4>Cabin List</h4>
-                        <router-link class="btn btn-primary menu-style" :to="{ name: 'cabin-add'}">Cabin Add</router-link> 
-                    </div>
-                    <div class="card-body">
-                        <!-- main work -->
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-7 m-auto">
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                        <th>Cabin No</th>
-                                        <th>Cabin Name</th>
-                                        <th>Cabin Details</th>
-                                        <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="item in cabinlist" :key="item.cabin_no">
-                                            <td>{{ item.cabin_no }}</td>
-                                            <td>{{ item.cabin_name }}</td>
-                                            <td>{{ item.cabin_description }}</td>
-                                            <td>
-                                                <router-link class="btn btn-primary shadow btn-xs sharp mr-1" style="color:#fff" :to="{ name: 'cabin-edit', params:{id:item.cabin_no} }">Edit</router-link>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- main part -->
-                    </div>
-                    
-                </div>
+  
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card custom__card">
+            <div class="card-header">
+              <h4>Cabin List</h4>
+              <router-link
+                class="btn btn-primary menu-style"
+                :to="{ name: 'cabin-add' }"
+                >Cabin Add</router-link
+              >
             </div>
+            <div class="card-body">
           
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-7 m-auto">
+                    <template>
+                      <v-data-table
+                        :headers="headers"
+                        :items="cabinlist"
+                        :items-per-page="5"
+                        item-key="name"
+                        class="elevation-1"
+                        loading
+                        loading-text="Loading... Please wait"
+                      ></v-data-table>
+                    </template>
+                  </div>
+                </div>
+              </div>
+             
+            </div>
+          </div>
         </div>
       </div>
-      <!-- main form -->
+    </div>
+ 
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Cabin ID",
+          value: "cabin_no",
+        },
+        {
+          text: "Cabin Name",
+          value: "cabin_name",
+        },
 
-    data(){
-      return{
-        cabinlist:[]
-      }
-    },
+        {
+          text: "Cabin Description",
+          value: "cabin_description",
+        },
+        {
+          text: "Action",
+          value: "cabin_description",
+          //   <router-link class="btn btn-primary shadow btn-xs sharp mr-1" style="color:#fff" :to="{ name: 'cabin-edit', params:{id:item.cabin_no} }">Edit</router-link>
+        },
+      ],
+      cabinlist: [],
+    };
+  },
 
-    async mounted() {
-      let result = await axios.get('http://127.0.0.1:8000/api/cabin');
-      this.cabinlist = result.data.data
-    },
-
-
-
-}
+  async mounted() {
+    let result = await axios.get("http://127.0.0.1:8000/api/cabin");
+    this.cabinlist = result.data.data;
+  },
+};
 </script>
